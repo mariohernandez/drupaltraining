4a20e705b58055847077e799d00ae1262b8b6c47
Mario Hernandez <mario1969@gmail.com>
2020-06-14 15:42:25 -0700

    Updates settings.local to override settings.ddev

---
+++ web/sites/default/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+// #ddev-generated: Automatically generated Drupal settings file.
+if (file_exists($app_root . '/' . $site_path . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 'true') {
+  include $app_root . '/' . $site_path . '/settings.ddev.php';
+}
+
+if (file_exists($app_root . '/' . $site_path . '/settings.local.php') && getenv('IS_DDEV_PROJECT') == 'true') {
+  include $app_root . '/' . $site_path . '/settings.local.php';
+}
