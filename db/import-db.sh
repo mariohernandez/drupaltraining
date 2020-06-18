ae4a877a174d03a102773dc8264a4f69df828cba
Mario Hernandez <mario1969@gmail.com>
2020-06-17 17:20:19 -0700

    Adds custom commands and dockerfile

---
+++ db/import-db.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Use a table that should exist in your database.
+if ! mysql -e 'SELECT * FROM node__field_hero;' db 2>/dev/null; then
+  echo 'Importing the database'
+  # Provide path to custom database.
+  gzip -dc /var/www/html/db/db.sql.gz | mysql db
+fi
