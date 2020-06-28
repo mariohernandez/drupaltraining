8e6dc60993f5f7de003c3e107d26968e67eb4589
Mario Hernandez <mario1969@gmail.com>
2020-06-27 21:43:07 -0700

    Renames db name

--- db/import-db.sh
+++ db/import-db.sh
@@ -4,5 +4,5 @@
 if ! mysql -e 'SELECT * FROM node__field_hero;' db 2>/dev/null; then
   echo 'Importing the database'
   # Provide path to custom database.
-  gzip -dc /var/www/html/db/db.sql.gz | mysql db
+  gzip -dc /var/www/html/db/drupaltraining.sql.gz | mysql db
 fi
